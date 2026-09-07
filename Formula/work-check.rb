2c915ea3f55e46b39c332ea27e74838d395ada76
Axel Talmet <axel@bjc.gg>
2026-09-07 11:38:59 +0100

    work-check 1.9.4: gossip gang last-heard + probability

--- Formula/work-check.rb
+++ Formula/work-check.rb
@@ -1,25 +1,25 @@
 class WorkCheck < Formula
   desc "tiny office check-in board"
   homepage "https://github.com/wombat-inc/homebrew-tap"
-  version "1.9.3"
+  version "1.9.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.3/work-check-darwin-arm64"
-      sha256 "55d2663ee44fd58d5e013be4586266c65099e03c96ad7cd72288c3734454cc64"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.4/work-check-darwin-arm64"
+      sha256 "f39ff7589014287f457854ec59e62864a41d02899f8c518e2a80bdf1776091f2"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.3/work-check-darwin-x64"
-      sha256 "59451c5baa17af6c5d38ec74cf628b161bda76895c6196bdf5201939fa6ff074"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.4/work-check-darwin-x64"
+      sha256 "2766062b603b04caec6ecba372c36ed7f70c5fbdd0807f41955b9dc1f34d0304"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.3/work-check-linux-x64"
-      sha256 "ec173a03f89846eadcac510ba63e8339367429ea41ab62a2d5f9a599f4f4c5ca"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.4/work-check-linux-x64"
+      sha256 "bf638ac4c1cb077d652eff62a8bfd5b9b00591cce9978493a8cd692ca6e51b15"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.3/work-check-linux-arm64"
-      sha256 "f9163b1fa7a0f926dcc28ee4a7fca4c5ab4af05186cd014810c1c5e42cc39fc7"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.4/work-check-linux-arm64"
+      sha256 "dd5cf1f3716359a9f9c26cf17a2e6fd97290be66dada96ee5d9dcae38d118624"
     end
   end
 
