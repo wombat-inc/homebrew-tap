763b26d7d055053c8c2dc46bc4b4af979f405102
Axel Talmet <axel@bjc.gg>
2026-09-07 16:30:43 +0100

    work-check 1.9.6: office trend

--- Formula/work-check.rb
+++ Formula/work-check.rb
@@ -1,25 +1,25 @@
 class WorkCheck < Formula
   desc "tiny office check-in board"
   homepage "https://github.com/wombat-inc/homebrew-tap"
-  version "1.9.5"
+  version "1.9.6"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.5/work-check-darwin-arm64"
-      sha256 "acdd7c33608ecb113feda637f30536ff3c91ada284012365b2e36b673a7a1878"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.6/work-check-darwin-arm64"
+      sha256 "c84ea047900d9d2f1b9ced5958a681b5a6ea4aa6c292f07e246d50069e0c31fe"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.5/work-check-darwin-x64"
-      sha256 "78c1e5b1f0d5d66a2433ba55f9de06b9633078b54b1393f460ccc36f7742c70c"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.6/work-check-darwin-x64"
+      sha256 "dc005216ae4e311a3726d596e30c03db56e0fe0a35cdebb91ea8e1f69b249fc0"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.5/work-check-linux-x64"
-      sha256 "4e2d039d50fe9cb34c9918ea438080d18b7ab1dcf22530e3d00954c5080375d2"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.6/work-check-linux-x64"
+      sha256 "1857c4c3fe3f0b099dbd57e8d1bf977ca96b56a125a0f72be0dfcd71b80ecfe4"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.5/work-check-linux-arm64"
-      sha256 "dce6528dbf555594f3426d9cea0d70deaed4522acddf834680494e2922a60999"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.6/work-check-linux-arm64"
+      sha256 "74c14312c12ad900ce045a3c88a56d21bd01ee8c23412f0bc3a4107cacd6d8bd"
     end
   end
 
