5d8516df3a6a161b01ddec61ad7a637570176876
Axel Talmet <a@bjc.gg>
2026-09-07 03:05:13 +0100

    work-check 1.4.0

--- Formula/work-check.rb
+++ Formula/work-check.rb
@@ -1,25 +1,25 @@
 class WorkCheck < Formula
   desc "tiny office check-in board"
-  homepage "https://github.com/bjcgg/work-check"
-  version "1.3.0"
+  homepage "https://github.com/wombat-inc/homebrew-tap"
+  version "1.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.3.0/work-check-darwin-arm64"
-      sha256 "4f9799a1777b0c461f21109959f4f482b039d12b9f2addba25c4591ff3c7b275"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.4.0/work-check-darwin-arm64"
+      sha256 "86e1c26ab59f9c9e32983ee772c02763bc5b9bcbeafba95324b6a53aad98bf66"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.3.0/work-check-darwin-x64"
-      sha256 "d498bb49406887c6d0f4cb04ca2ee7aa26d5078644896eec0fedb3601428ee6c"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.4.0/work-check-darwin-x64"
+      sha256 "12b6cbc0ea216dac0ad74012d5eab1a1065891029aa9583b53501021f2cf1d71"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.3.0/work-check-linux-x64"
-      sha256 "4ccd87cf2527442bc153f17358d533ccfba7039a810fce18879381db77197d73"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.4.0/work-check-linux-x64"
+      sha256 "b338f2870f4bf1d795c5d3274b278ee366e305c53c6c75dc232ba2602593fb81"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.3.0/work-check-linux-arm64"
-      sha256 "10334daf97d6f1b650b2b7fc2df3a6f31ab14215ae5aa527fb29bfc50668178a"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.4.0/work-check-linux-arm64"
+      sha256 "2a2e64eba70252c9273cc01ba15985b5ff9ffe70c948c8f8a946c9df7d022021"
     end
   end
 
