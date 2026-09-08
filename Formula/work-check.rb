3b94088b168cf8bf303ecc5eecda26e33e39edeb
Axel Talmet <axel@bjc.gg>
2026-09-08 02:48:05 +0100

    work-check 1.9.8: loudest gossiper in

--- Formula/work-check.rb
+++ Formula/work-check.rb
@@ -1,25 +1,25 @@
 class WorkCheck < Formula
   desc "tiny office check-in board"
   homepage "https://github.com/wombat-inc/homebrew-tap"
-  version "1.9.7"
+  version "1.9.8"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.7/work-check-darwin-arm64"
-      sha256 "da1015a31608f88168be4644789fe75ec000f3137d1ea2e37aa768f7b69500f0"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.8/work-check-darwin-arm64"
+      sha256 "36f9a12953febc1f86b7cd0d295a63a96c4f9f6a832441a90356d01c8e47466f"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.7/work-check-darwin-x64"
-      sha256 "a2de5fb186d012fddeeb9d8279d228e01b5fa77f0a0cd16e110d9809b3d6fdab"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.8/work-check-darwin-x64"
+      sha256 "24e7834351ac63825feaabcb115b0e694794fe892bf76201ca3b53a068546cf6"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.7/work-check-linux-x64"
-      sha256 "65dddac82a38e26b4c7103edcb6205346dbde1fed3e49e734750ce726c5e6d6c"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.8/work-check-linux-x64"
+      sha256 "554e00c2aa6c6b50a3adef14a308e496300403e1dcf7d6b136cd2a74d9c04ebd"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.7/work-check-linux-arm64"
-      sha256 "9378854f6338f12048748f7d05e80e2d54972f2e2a3e2d26504dfe17a580a8e0"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v1.9.8/work-check-linux-arm64"
+      sha256 "194d2b1386818af6ee8f56e35ff7793db041d971a90a9e313daadf81d4e43f3d"
     end
   end
 
