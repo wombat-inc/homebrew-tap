b49b518fa6486d193e30cf11cace6a81370bd47c
Axel Talmet <axel@bjc.gg>
2026-09-09 01:13:43 +0100

    work-check 2.0.3

--- Formula/work-check.rb
+++ Formula/work-check.rb
@@ -1,25 +1,25 @@
 class WorkCheck < Formula
   desc "tiny office check-in board"
   homepage "https://github.com/wombat-inc/homebrew-tap"
-  version "2.0.2"
+  version "2.0.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.2/work-check-darwin-arm64"
-      sha256 "e12da86768c741517adde5ac24ec8fe4d23e9226cd40ff31472b7841b3835cd5"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.3/work-check-darwin-arm64"
+      sha256 "0ac63ed141df0e5a7dfdacf129a596e3cdcf3342e50ab9de9a612f49a97635f1"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.2/work-check-darwin-x64"
-      sha256 "8d0e07d13e1fc5da9518ef4533b77c6f0f6865e58858b6681cde224f4db7ede9"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.3/work-check-darwin-x64"
+      sha256 "e36206d7a0def1406a45dbcd9c7e24afdf1459430fb34440072653e14d689d86"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.2/work-check-linux-x64"
-      sha256 "89752d5a410715efb62a61a0a61231a7f30612540db875af15aa79057ec0dc07"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.3/work-check-linux-x64"
+      sha256 "9cafbf45d9c35d5e18e1d737359b085976a011c0add6044cf1af2ebaf85a324b"
     else
-      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.2/work-check-linux-arm64"
-      sha256 "9f9a4efa9ecc28deb0c4a20be949631feae74f73c54e11d25375b7ab55218de5"
+      url "https://github.com/wombat-inc/homebrew-tap/releases/download/work-check-v2.0.3/work-check-linux-arm64"
+      sha256 "7bfcbd08cfe28cc928be2a85bff180614b5299eda307095622a6f2b24aa2c3aa"
     end
   end
 
